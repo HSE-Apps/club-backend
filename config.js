31a950be6b150e2d98660f961be3193d1ca7ceb0
Preston <51501899+PrestonMcCrary@users.noreply.github.com>
2020-08-30 21:40:03 -0400

    Announcement Feature Cleanup

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
 
-const client = new SecretManagerServiceClient({keyFile: './key.json'})
+const client = new SecretManagerServiceClient()
 
 exports.config = {}
 
