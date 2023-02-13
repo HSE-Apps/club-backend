fef1463670bbf6d817a19b7b4a721ba1505d47a3
Lakshay Mahajan <lucky03072006@gmail.com>
2023-02-13 12:27:22 -0500

    made it work

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
-const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
+/* const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
 
-const client = new SecretManagerServiceClient()
+const client = new SecretManagerServiceClient() */
 
 exports.config = {}
 
 exports.setConfig = async () => {
-
+/* 
     try{
         const [unParsed] = await client.accessSecretVersion({name: 'projects/hse-clubs/secrets/MONGO_URI/versions/latest'})
         
@@ -14,5 +14,7 @@ exports.setConfig = async () => {
 
     } catch(err) {
         console.log(err) 
-    }
+    } */
+
+    exports.config.MONGO_URI = "mongodb+srv://gcp:zO5PH8qquB5nP9De@cluster0.ikms4zd.mongodb.net/ClubData?retryWrites=true&w=majority"
 }
